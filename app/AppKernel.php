33412af76aaf0ba409028b94a268c560f3a667db
Nathan Williford <nathan.williford@gmail.com>
2012-09-15 19:53:57 -0700

    Added AvalancheImageineBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Nate\PortfolioBundle\NatePortfolioBundle(),
+            new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
