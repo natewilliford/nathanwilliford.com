bc97a1003eba536bfd6aa0647230dd21ba1e3749
Nathan Williford <nathan.williford@gmail.com>
2012-09-20 15:54:03 -0700

    Design and UX changes

--- web/bundles/nateportfolio/js/main.js
+++ web/bundles/nateportfolio/js/main.js
@@ -2,3 +2,18 @@
 $(window).scroll(function(e) {
     $('body').css('backgroundPositionY', - $(this).scrollTop() / 3);
 });
+
+$(document).ready(function(){
+    setTimeout(function(){ update_bottom_padding(); }, 5000);
+});
+
+$(window).resize(function(e) {
+    update_bottom_padding();
+});
+
+function update_bottom_padding() {
+    console.log('doin it');
+    var magick = 400;
+    var padding = $(window).height() - $('.portfolio-entry-item:last').height() - magick;
+    $('#content .col2').css('paddingBottom', padding);
+}
