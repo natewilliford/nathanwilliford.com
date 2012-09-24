4b5031917434657419efb25d3f6925964d05af8b
Nathan Williford <nathan.williford@gmail.com>
2012-09-24 11:11:38 -0700

    Updated for a little better mobile experience

--- web/js/7deb865.js
+++ web/js/7deb865.js
@@ -353,37 +353,68 @@ lightbox = new Lightbox options
 }).call(this);
 
 
-$(window).scroll(function(e) {
-    // Parallax scrolling
-    var scrollTop = $(this).scrollTop();
-    $('body').css('backgroundPositionY', - scrollTop / 20);
+var Site = (function(){
 
-    update_card_alpha(scrollTop);
-});
+    var self = this;
+    var isMobile;
 
-$(document).ready(function(){
-    setTimeout(function(){ update_bottom_padding(); }, 5000);
-    update_card_alpha();
-});
+    this.isMobile = function() {
+        if (isMobile == null) {
+            isMobile = navigator.userAgent.match(/Android/i)
+                || navigator.userAgent.match(/BlackBerry/i)
+                || navigator.userAgent.match(/iPhone|iPad|iPod/i)
+                || navigator.userAgent.match(/IEMobile/i);
+        }
+        return isMobile;
+    };
 
-$(window).resize(function(e) {
-    update_bottom_padding();
-});
+    $(document).ready(function(){
+        if (self.isMobile()) {
+            $('body').addClass('mobile');
+            $('.portfolio-entry-item').css('opacity', 1);
+            $('[rel="lightbox"]').attr('rel', 'not_lightbox');
 
-function update_bottom_padding() {
-    var magick = 360;
-    var padding = $(window).height() - $('.portfolio-entry-item:last').height() - magick;
-    $('#content .col2').css('paddingBottom', padding);
-}
-
-function update_card_alpha(scrollTop) {
-    $('.portfolio-entry-item').each(function() {
-        var distance = Math.abs(scrollTop - $(this).offset().top + 200);
-
-        if (distance > 0) {
-            $(this).css('opacity', 300/distance);
-        } else  {
-            $(this).css('opacity', 1);
+        } else {
+            updateBottomPadding();
+            updateCardAlpha(0);
         }
     });
-}
+
+    $(window).scroll(function(e) {
+        if (!self.isMobile()) {
+            var scrollTop = $(this).scrollTop();
+            updateParallax(scrollTop);
+            updateCardAlpha(scrollTop);
+        }
+    });
+
+    $(window).resize(function(e) {
+        if (!self.isMobile()) {
+            updateBottomPadding();
+        }
+    });
+
+    var updateBottomPadding = function() {
+        var magic = 360;
+        var padding = $(window).height() - $('.portfolio-entry-item:last').height() - magic;
+        $('#content .col2').css('paddingBottom', padding);
+    };
+
+    var updateCardAlpha = function(scrollTop) {
+        $('.portfolio-entry-item').each(function() {
+            var distance = Math.abs(scrollTop - $(this).offset().top + 200);
+
+            if (distance > 0) {
+                $(this).css('opacity', 300/distance);
+            } else  {
+                $(this).css('opacity', 1);
+            }
+        });
+    };
+
+    var updateParallax = function(scrollTop) {
+        $('body').css('backgroundPositionY', - scrollTop / 20);
+    };
+
+})();
+
